11a647bb29e1b9b5b5c0594f7f4f25961adee793
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2022-06-01 15:45:39 +0600

    add all file

--- asscet/js/main.js
+++ asscet/js/main.js
@@ -1,1 +1,38 @@
-{\rtf1}
+
+  (function($) {
+    "use strict";
+
+
+
+    //Sticky Menu
+    $(window).scroll(function() {
+      var Width = $(document).width();
+
+      if ($("body").scrollTop() > 100 || $("html").scrollTop() > 100) {
+          if (Width > 767) {
+              $(".header-wapper").addClass("sticky");
+          }
+      } else {
+          $(".header-wapper").removeClass("sticky");
+      }
+    });
+
+
+
+      // slick
+  $('.slider-man-image').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-testimonials'
+  });
+  $('.slider-testimonials').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-man-image',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+})(jQuery); // End jQuery
