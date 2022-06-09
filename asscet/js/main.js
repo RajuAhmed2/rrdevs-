fc10af231ec75a0fd886f559c41ae2dffd20604e
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2022-06-09 17:12:51 +0600

    finish work

--- asscet/js/main.js
+++ asscet/js/main.js
@@ -2,7 +2,18 @@
   (function($) {
     "use strict";
 
-
+    setTimeout(function() {
+      $('#ctn-preloader').addClass('loaded');
+      // Una vez haya terminado el preloader aparezca el scroll
+      $('body').removeClass('no-scroll-y');
+  
+      if ($('#ctn-preloader').hasClass('loaded')) {
+        // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
+        $('#preloader').delay(1000).queue(function() {
+          $(this).remove();
+        });
+      }
+    }, 3000);
 
     //Sticky Menu
     $(window).scroll(function() {
@@ -16,23 +27,55 @@
           $(".header-wapper").removeClass("sticky");
       }
     });
-
-
-
       // slick
   $('.slider-man-image').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed:3000,
     arrows: false,
     fade: true,
     asNavFor: '.slider-testimonials'
   });
-  $('.slider-testimonials').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: '.slider-man-image',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
+    $('.slider-testimonials').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.slider-man-image',
+      dots: true,
+      centerMode: true,
+      autoplay: true,
+      autoplaySpeed:3000,
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
 })(jQuery); // End jQuery
+
+
+const navBar_button = document.getElementById('nav_bar');
+const time_button = document.getElementById('nav-mob');
+const overly = document.getElementById('overly');
+
+navBar_button.addEventListener('click', () => {
+  overly.classList.add('active');
+});
+
+time_button.addEventListener('click', () => {
+  overly.classList.remove('active');
+});
